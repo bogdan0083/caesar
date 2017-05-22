3ec24e06b6f2e71629855e03f4faffa9213cfade
Bogdan Dolin <d.bogdan@inbox.ru>
2017-05-23 00:28:07 +0700

    progress 2nd page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,6 +15,7 @@ $(document).ready(function () {
   var hoverReadyClass = 'js-hover-ready';
   var $anchors = $('[data-anchor]');
   var $circleItems = $('.circle-item');
+  var $tabs = $('.js-tab-pane');
 
   // media breakpoints
   var screenXs = 460;
@@ -69,6 +70,18 @@ $(document).ready(function () {
     $mouseIcon.removeClass('hidden');
   }
 
+  // Fires when clicking the tab pane
+  function onTabPaneClick(e) {
+    e.preventDefault();
+    var $target = $(this);
+    var idx = $target.index();
+    var $parent = $target.parent();
+    var $tabs = $parent.find('.js-tab-pane');
+    var $slider = $( $parent.data('slider') );
+    $tabs.removeClass('js-active');
+    $target.addClass('js-active');
+    $slider.slick('slickGoTo', idx);
+  }
   // animate frontpage block on hover
   if ($promo.length > 0 && windowWidth > screenSm) {
     // allow hovering on promo items only after 1 second
@@ -87,11 +100,177 @@ $(document).ready(function () {
     });
   });
 
-  $circleItems.hover(function (e) {
-    var width = $(this).width();
-    var height = $(this).height();
-    console.log(width);
-  }, function (e) {
+  // Tabs event handler
+  $tabs.click(onTabPaneClick);
 
+  // SLIDERS -------------------------
+
+  // about object slider
+  var $aboutObjectSlider = $('.about-object-slider');
+
+  $aboutObjectSlider.on('init', function (slick) {
+    AOS.refresh();
+    Waypoint.refreshAll();
   });
+
+  // $('body').imagesLoaded(function () {
+  // AOS.refresh();
+  // Waypoint.refreshAll();
+  // });
+
+  $aboutObjectSlider.slick({
+    autoplay: false,
+    swipe: false,
+    infinite: false,
+    arrows: false,
+    dots: false,
+    adaptiveHeight: true
+  });
+
+  // about object slider
+  var $gallery = $('.gallery-slider .wrapper');
+
+  $gallery.on('init', function (slick) {
+    AOS.refresh();
+    Waypoint.refreshAll();
+  });
+
+  $gallery.slick({
+    autoplay: true,
+    infinite: true,
+    arrows: true,
+    dots: false,
+    adaptiveHeight: true,
+    prevArrow: $('.gallery-slider .arr-left'),
+    nextArrow: $('.gallery-slider .arr-right'),
+    mobileFirst: true,
+    responsive: [
+    {
+      breakpoint: 880,
+      settings: {
+        slidesToShow: 2
+      }
+
+    }
+    ]
+  });
+
+  // about object slider
+  var $featuresSlider = $('.object-features-slider .wrapper');
+
+  $featuresSlider.on('beforeChange', function(_, slick, current, next) {
+    var $currentSlide = $(slick.$slides[current]);
+    var $nextSlide = $(slick.$slides[next]);
+    $('.object-features-slider .from').text( next + 1 );
+  });
+
+  $featuresSlider.on('init', function (slick) {
+    AOS.refresh();
+    Waypoint.refreshAll();
+  });
+
+  $featuresSlider.slick({
+    autoplay: true,
+    infinite: true,
+    arrows: true,
+    dots: false,
+    adaptiveHeight: true,
+    prevArrow: $('.object-features-slider .arr-left'),
+    nextArrow: $('.object-features-slider .arr-right'),
+    mobileFirst: true,
+    adaptiveHeight: true,
+    centerMode: true,
+    autoplaySpeed: 6000,
+    responsive: [
+    {
+      breakpoint: screenSm,
+      settings: {
+        centerMode: true,
+        autoplaySpeed: 6000,
+        slidesToShow: 3
+      }
+
+    },
+    {
+      breakpoint: screenLg,
+      settings: {
+        centerMode: true,
+        autoplaySpeed: 6000,
+        slidesToShow: 5
+      }
+
+    }
+    ]
+  });
+  // END SLIDERS ---------------------
+  //
+  function isInt(n){
+    return Number(n) === n && n % 1 === 0;
+  }
+
+  function isFloat(n){
+    return Number(n) === n && n % 1 !== 0;
+  }
+
+  function animateNumbers() {
+    // animate numbers in object-distance section
+    var $distanceItem = $('.distance-item');
+
+    $distanceItem.each(function (idx, item) {
+      var $item = $(item);
+      var $numElem = $item.find('.num');
+      var from = $numElem.data('from');
+      var to = $numElem.data('to');
+      // if number is integer, animate it like int
+      if ( isInt(from) ) {
+
+        $numElem.prop('number', parseInt(from)).animateNumber({
+          number: parseInt(to),
+          easing: 'easeOutQuad'
+        }, 3000)
+
+      } else if ( isFloat(from) ) {
+        // if number is float, animate it like float
+        var decimal_places = 1;
+        var decimal_factor = decimal_places === 0 ? 1 : Math.pow(10, decimal_places);
+
+        $numElem.prop('number', parseFloat(from))
+          .animateNumber({
+            number: parseFloat(to) * decimal_factor,
+            easing: 'easeOutQuad',
+            numberStep: function(now, tween) {
+              var floored_number = Math.floor(now) / decimal_factor,
+              target = $(tween.elem);
+
+              if (decimal_places > 0) {
+                // force decimal places even if they are 0
+                floored_number = floored_number.toFixed(decimal_places);
+              }
+
+              target.text(floored_number);
+            }
+          }, 3000)
+      }
+    });
+  }
+
+// ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ
+
+(function initMap() {
+  if ($('.google-map').length > 0) {
+
+    var myLatLng = {
+      lat: 55.777056,
+      lng: 37.527264
+    }
+
+    var map = new google.maps.Map(document.querySelector('.google-map'), {
+      center: myLatLng,
+      zoom: 14
+    });
+    var marker = new google.maps.Marker({position: myLatLng, title: "Hello World!", icon: 'images/logo-shadow.png'});
+    marker.setMap(map);
+
+  }
+})();
 });
